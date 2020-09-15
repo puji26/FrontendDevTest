6cca02b81fe3df70e528385f4e2c65c801487aa7
Puji Wahono <pwahono26@gmail.com>
2020-09-15 23:05:14 +0700

    Create Project Employee

---
+++ 2. Mobile App for CRUD REST API/Employee/Employee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employee
+//
+//  Created by Puji Wahono on 15/09/20.
+//  Copyright © 2020 Puji Wahono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
